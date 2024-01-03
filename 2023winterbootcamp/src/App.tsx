2950167e3691dba48b34801484250557beb530e6
Ahnnakyung <nakyung.ahn.03@gmail.com>
2024-01-03 22:28:30 +0900

    feat:푸터 설정, 모달 부분 일부 수정

--- 2023winterbootcamp/src/App.tsx
+++ 2023winterbootcamp/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Layout from "./components/Layout";
 import Main from "./Mainpage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Layout>
       <Main />
+      <Footer />
     </Layout>
-  )
+  );
 }
 
 export default App;
