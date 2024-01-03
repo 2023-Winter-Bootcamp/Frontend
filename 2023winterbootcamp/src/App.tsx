d950b75be49b038a575041eaf0a224536e6f0894
Juhan Park <iiiamst16@gmail.com>
2024-01-04 02:25:10 +0900

    feat: 마이페이지 퍼블리싱

--- 2023winterbootcamp/src/App.tsx
+++ 2023winterbootcamp/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Mypage from "./Mypage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Mypage/>
     </div>
   );
 }
